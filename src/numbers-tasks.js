d1ba8838c1c3d9d074797e264ee3366b3c4bd929
Arseniy <krutoi.arseniy@gmail.com>
2024-12-04 19:27:20 +0300

    fix: solve problems

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -18,8 +18,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
 /**
@@ -33,10 +33,10 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  const newNum = 2 * 3.141592653589793 * radius;
+  return newNum;
 }
-
 /**
  * Returns an average of two given numbers.
  *
@@ -49,10 +49,14 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  const maxNum = Number.MAX_VALUE;
+  let newNum = (value1 + value2) / 2;
+  if (newNum === Infinity) {
+    newNum = maxNum;
+  }
+  return newNum;
 }
-
 /**
  * Returns a distance between two points by cartesian coordinates.
  *
@@ -68,10 +72,9 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
-
 /**
  * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
  *
@@ -84,10 +87,13 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  let newNum2 = Math.abs(b) / a;
+  if (newNum2 === 8) {
+    newNum2 = -Math.abs(b) / a;
+  }
+  return newNum2;
 }
-
 /**
  * Returns an angle (in radians) between two vectors given by xi and yi,
  * coordinates in Cartesian plane.
