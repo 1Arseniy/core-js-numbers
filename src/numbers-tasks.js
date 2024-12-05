44cdbe6c9dc10f48c230525d08d380a174436c6b
Arseniy <krutoi.arseniy@gmail.com>
2024-12-05 21:00:04 +0300

    fix: solve new problems

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -111,8 +111,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const num2 = x1 * x2 + y1 * y2;
+  const numModal1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const numModal2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const newNum = num2 / (numModal1 * numModal2);
+  return Math.acos(newNum);
 }
 
 /**
@@ -128,8 +132,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -143,8 +147,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -160,10 +164,10 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const lengthDiagonal = a ** 2 + b ** 2 + c ** 2;
+  return Math.sqrt(lengthDiagonal);
 }
-
 /**
  * Returns the number rounded to specified power of 10.
  *
@@ -181,8 +185,10 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const numPow = 10 ** pow;
+  const numRound = Math.round(Math.round(num) / numPow) * numPow;
+  return numRound;
 }
 
 /**
